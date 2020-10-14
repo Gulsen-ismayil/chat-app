18cf247ba6903fcf74b4a3973815f64b79782207
GS <gulixiansimayi@gmail.com>
2020-10-14 11:52:58 +0200

    create new repository

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
+const online = true;
+const name = "Atacey Reed";
+const image = "https://randomuser.me/api/portraits/women/21.jpg";
 
+function Contact() {
+  return (
+    <div className="Contact">
+      <img className="avatar" alt="Sracey Reed" src={image} />
+
+      <div className="status">
+        <h4 className="name">{name}</h4>
+        <div className="status-online"></div>
+        <p className="status-text">{online ? "online" : "offline"} </p>
+      </div>
+    </div>
+  );
+}
 
 export default Contact;
-
-
-
-
-
-
-
